0685afd7887d99edc7721a593ab3816aec44d532
a1668077 <a1668077@adelaide.edu.au>
2016-10-09 08:03:21 +0000

    Handle sending notifications from command line

--- src/clients/CommandLineNotificationServer.java
+++ src/clients/CommandLineNotificationServer.java
@@ -2,6 +2,8 @@ package clients;
 
 import java.io.IOException;
 import java.net.URISyntaxException;
+import java.util.ArrayList;
+import java.util.Arrays;
 import java.util.List;
 import java.util.Scanner;
 import java.util.UUID;
@@ -13,6 +15,7 @@ import java.util.concurrent.TimeUnit;
 import java.util.concurrent.TimeoutException;
 
 import util.Notification;
+import util.Severity;
 
 public class CommandLineNotificationServer {
   public CommandLineNotificationServer(HttpClient httpClient, int retries, int timeout) {
@@ -25,53 +28,101 @@ public class CommandLineNotificationServer {
     this(new HttpClient(requestRoot), retries, timeout);
   }
 
-  public void register() throws TimeoutException, IOException {
-    Future<UUID> success = null;
-
-    for (int i = 0; i < retries; i++) {
+  public void send() {
+    Scanner input = new Scanner(System.in);
+    Notification notification = new Notification();
+    notification.timestamp = System.currentTimeMillis();
+    System.out.println("Message: ");
+    notification.message = input.nextLine();
+    notification.sender = senderName;
+    System.out.println("Location: ");
+    notification.location = input.nextLine();
+    System.out.println("Severity: ");
+    while (notification.severity == null) {
       try {
-        success = httpClient.register();
-        success.get(timeout, TimeUnit.MILLISECONDS);
-        return;
-      } catch (InterruptedException | ExecutionException | TimeoutException exception) {
-        success.cancel(true);
-        success = httpClient.register();
+        notification.severity = Severity.fromString(input.next());
+      } catch (IllegalArgumentException illegal) {
+        System.out.println(illegal.getMessage() + " Try again.");
       }
     }
 
-    success.cancel(true);
-    throw new TimeoutException("Server didn't respond in time. Try again.");
+    try {
+      Future<Notification> created = httpClient.sendNotification(notification);
+      for (int i = 0; i < retries; i++) {
+        try {
+          if (created.get(timeout, TimeUnit.MILLISECONDS) != null) {
+            System.out.println("Successfully sent");
+            return;
+          }
+        } catch (InterruptedException | ExecutionException | TimeoutException e) {
+          created.cancel(true);
+          created = httpClient.sendNotification(notification);
+        }
+      }
+      throw new TimeoutException("Failed to connect after retrying.");
+    } catch (IOException | TimeoutException e) {
+      System.out.println("Server problem, try again.");
+    }
+  }
+
+  public void register() {
+    Future<UUID> success = null;
+
+    try {
+      for (int i = 0; i < retries; i++) {
+        try {
+          success = httpClient.register();
+          success.get(timeout, TimeUnit.MILLISECONDS);
+          return;
+        } catch (InterruptedException | ExecutionException | TimeoutException exception) {
+          success.cancel(true);
+          success = httpClient.register();
+        }
+      }
+
+      throw new TimeoutException("Server didn't respond in time. Try again.");
+    } catch(IOException | TimeoutException exception) {
+      success.cancel(true);
+      System.err.println("Server problem, try again.");
+    }
+  }
+
+  public void setName(String senderName) {
+    this.senderName = senderName;
   }
 
   public static void main(String[] args) throws TimeoutException, IOException, URISyntaxException {
     CommandLineNotificationServer client = new CommandLineNotificationServer(args[0],
                                                   Integer.parseInt(args[1]),
                                                   Integer.parseInt(args[2]));
-    try {
-      client.register();
-    } catch (IOException | TimeoutException e) {
-      System.err.println("System error. Try again later");
-      System.exit(1);
-    }
+    client.register();
 
     Scanner input = new Scanner(System.in);
-    System.out.println("Client ready to go.");
+
+    System.out.println("Give yourself a name: ");
+    client.setName(input.nextLine());
+
+    System.out.println("Ready to go.");
+    System.out.print("> ");
 
     while(input.hasNextLine()) {
       String line = input.nextLine();
-      String[] components = line.split(" ");
-      switch (components[0]) {
+      ArrayList<String> components = new ArrayList<>(Arrays.asList(line.split(" ")));
+      switch (components.get(0)) {
         case "send":
-          client.send(components.);
+          client.send();
           break;
         default:
           System.out.println("Not sure what you mean. Try " + possibleInputs);
       }
+
+      System.out.print("> ");
     }
   }
 
-  static String possibleInputs = "send";
-  int timeout;
-  int retries;
-  HttpClient httpClient;
+  private static String possibleInputs = "send";
+  private int timeout;
+  private int retries;
+  private String senderName;
+  private HttpClient httpClient;
 }
--- src/util/Severity.java
+++ src/util/Severity.java
@@ -8,6 +8,19 @@ public enum Severity {
   @SerializedName("caution")
   CAUTION,
   @SerializedName("urgent")
-  URGENT
+  URGENT;
+
+  public static Severity fromString(String severity) {
+    switch (severity) {
+      case "notice":
+        return NOTICE;
+      case "caution":
+        return CAUTION;
+      case "urgent":
+        return URGENT;
+      default:
+        throw new IllegalArgumentException("Expected one of notice, caution and urgent");
+    }
+  }
 
 }
